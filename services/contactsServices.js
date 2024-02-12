1cc70296916b6bf8bb15d0bd3b3420f0a591551c
Kirra-Potiekhina <kirrapotehina@gmail.com>
2024-02-12 23:43:35 +0200

    finished controllers functions. not optimized yet

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -2,24 +2,24 @@ import fs from "fs/promises";
 import { nanoid } from "nanoid";
 import path from "path";
 
-const contactsPath = path.resolve("src/db", "contacts.json");
+const contactsPath = path.resolve("db", "contacts.json");
 function dataRewrite(contacts) {
   fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 }
 
-export async function listContacts() {
+export async function contactsList() {
   const data = await fs.readFile(contactsPath, "utf-8");
   return JSON.parse(data);
 }
 
 export async function getContactById(contactId) {
-  const dataById = await listContacts();
+  const dataById = await contactsList();
   const contactById = dataById.find((item) => item.id === contactId);
   return contactById || null;
 }
 
 export async function removeContact(contactId) {
-  const dataForDelete = await listContacts();
+  const dataForDelete = await contactsList();
   const contactToDelete = dataForDelete.findIndex(
     (item) => item.id === contactId
   );
@@ -27,12 +27,12 @@ export async function removeContact(contactId) {
     return null;
   }
   const dataUpdated = dataForDelete.splice(contactToDelete, 1);
-  await dataRewrite(dataForDelete);
+  dataRewrite(dataForDelete);
   return dataUpdated;
 }
 
 export async function addContact(name, email, phone) {
-  const dataForAdd = await listContacts();
+  const dataForAdd = await contactsList();
   const contactNew = {
     id: nanoid(),
     name,
@@ -42,4 +42,15 @@ export async function addContact(name, email, phone) {
   dataForAdd.push(contactNew);
   await dataRewrite(dataForAdd);
   return contactNew;
+}
+
+export async function updateContactById(id, data) {
+  const contacts = await contactsList();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { ...contacts[index], ...data };
+  dataRewrite(contacts);
+  return contacts[index];
 }
